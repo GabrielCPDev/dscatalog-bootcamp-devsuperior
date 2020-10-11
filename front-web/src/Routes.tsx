57f63d1c7b0708c9922e8a768c8a99426a51494c
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-10-11 15:24:07 -0300

    adding ProductPrice

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './core/components/Navbar';
 import Adimin from './pages/Admin';
 import Catalog from './pages/Catalog';
+import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
 
 const Routes = () => (
@@ -12,8 +13,11 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home></Home>
             </Route>
-            <Route path="/catalog">
+            <Route path="/products" exact>
                 <Catalog></Catalog>
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
             </Route>
             <Route path="/admin">
                 <Adimin></Adimin>
